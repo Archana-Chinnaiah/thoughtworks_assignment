44a693f5ff0c21d046fbbe4ba3342ca8efe4ac0f
Archana C <archanachinnaiah@gmail.com>
2020-07-10 02:36:32 -0700

    finding average using function

---
+++ avg_using_function.py
@@ -0,0 +1,19 @@
+'''def avg(name,*marks):
+    sum_num=0
+    for mark in marks:
+        sum_num=sum_num+mark
+    avg = sum_num / len(marks)
+    print name,"-",avg
+avg("Mala",80,96,84,70,56)
+'''
+import math
+def rage(*num):
+    s=avg(*num)
+    return avg
+print(rage(18,25,3,41,5))
+
+
+''' sum_num = 0
+   for t in num:
+        sum_num = sum_num + t           
+    avg = sum_num / len(num)'''
