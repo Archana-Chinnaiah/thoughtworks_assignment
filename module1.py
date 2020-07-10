384cb4bda18574e4f84ba91e81d307abb98be4f1
Archana C <archanachinnaiah@gmail.com>
2020-07-10 02:34:55 -0700

    module assignment

---
+++ module1.py
@@ -0,0 +1,6 @@
+def name_val(name):
+    return name
+def x_val(x):
+    return x
+def y_val(y):
+    return y
---
+++ module2.py
@@ -0,0 +1,7 @@
+from module1 import *
+name="archu"
+x=10
+y=20
+print"Hi,this is",name_val(name)
+print "x:",x_val(x)
+print "y:",y_val(y)
