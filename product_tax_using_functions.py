c4a846a03aab79f7247d0631d90ceb6f9a069ae2
Archana C <archanachinnaiah@gmail.com>
2020-07-16 21:28:32 +0530

    Find the tax for a product using function

--- product_tax_using_functions.py
+++ product_tax_using_functions.py
@@ -14,7 +14,7 @@ def shop_list(id,name,price,category):
          if(category.lower()=="textiles"):
              tax=tax+(price*(0.01))
          print("The Tax for the product",name," is",tax)
-     elif(price<500):
+     elif(price<=500):
          tax=0
          tax=tax+(price*0.02)
          if(category.lower()=="textiles"):
@@ -24,7 +24,7 @@ def shop_list(id,name,price,category):
   
 shop_list(11,"Milk",100,"dairy")
 shop_list(12,"butter",2000,"Dairy")
-shop_list(15,"butter",5000,"Dairy")
+shop_list(15,"chocolate",5000,"Dairy")
 shop_list(13,"saree",3000,"Textiles")
 shop_list(14,"T-shirt",490,"Textiles")
 shop_list(15,"Fruits",200,"Produce")
@@ -32,7 +32,7 @@ shop_list(16,"Vegetables",200,"Produce")
 shop_list(17,"Fan",200,"Home Needs")
 shop_list(18,"T.v",200,"Home Needs")
 
-# USER INPUT
+# GETTING USER INPUT
 '''
 id=int(input("Enter the ID for the Product: "))
 name=input("Enter the Name for the Product: ")
@@ -40,7 +40,3 @@ price=float(input("enter the price for the product"))
 category=input("enter the Category that the product belongs(DAIRY,PRODUCE,HOME NEEDS,TEXTILES): ")
 shop_list(id,name,price,category)
 '''
-
-
-
-   
