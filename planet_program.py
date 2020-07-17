22f6abe4216cc7fde6e1d64ab4b2697c3c3e80d4
Archana-Chinnaiah <66065120+Archana-Chinnaiah@users.noreply.github.com>
2020-07-17 19:30:07 +0530

    Update planet_program.py

--- planet_program.py
+++ planet_program.py
@@ -1,40 +1,42 @@
-class Planets:
+class Planet:    
     def __init__(self,name,diameter,moons,year):
         self.name=name
         self.dia=diameter
-        self.moon=moons
-        self.days=year
+        self.no_of_moons=moons
+        self.length_of_days=list(year.split(" "))
 
     def radius(self):
-        radii=0
-        radii=radii+(self.dia/2)
-        return radii
+        return self.dia/2
+
     def no_of_days(self):
-        days=0
-        days=days+(self.days)
-        return days
-    
-def large_planet(*planets):
-    large=planets[0].dia
-    for i in planets:
-        if i.dia>large:
-            large=i.dia
-    for i in planets:
-        if i.dia ==large:
-           print("the largest planet is: ",i.name) 
+        if(self.length_of_days[1].lower()=="earth"):
+            print("the number.of.days of planet",self.name," is: ",int(self.length_of_days[0])*365.25,"days")
+        else:
+           print("the number of days of the planet",self.name,"is: ",self.length_of_days[0],"days")
         
 
+    
+def large_planet(*planet):
+    large=planet[0].dia
+    for i in planet:
+        if i.dia>large:
+            large=i.dia
+            large_planet=i
+    print("the largest planet is: ",large_planet.name) 
+        
+    
+mercury=Planet("MERCURY",4879,0,"88 days")
+venus=Planet("VENUS",12100,0,"225 days")
+earth=Planet("EARTH",12755,1,"365 days")
+mars=Planet("MARS",6786,2,"687 days")
+jupiter=Planet("JUPITER",142800,79,"12 earth years")
+saturn=Planet("SATURN",120537,82,"29.5 earth years")
+uranus=Planet("URANUS",51819,27,"84 earth years")
+neptune=Planet("NEPTUNE",49529,14,"165 earth years")
+print("The radius of the  planet is: ",neptune.radius())
+jupiter.no_of_days()
+large_planet(mercury,venus,earth,mars,jupiter,saturn,uranus,neptune)    
+    
 
-mer=Planets("MERCURY",4879,0,88)
-v=Planets("VENUS",12100,0,225)
-e=Planets("EARTH",12755,1,365)
-m=Planets("MARS",6786,2,687)
-j=Planets("JUPITER",142800,79,4383)
-s=Planets("SATURN",120537,82,10774.875)
-u=Planets("URANUS",51819,27,30681)
-n=Planets("NEPTUNE",49529,14,60266.25)
-large_planet(mer,v,e,m,j,s,u,n)
-print("The radius of the neptune planet is: ",n.radius())
-print("the number.of.days of planet Jupiter: ",j.no_of_days())
     
     
