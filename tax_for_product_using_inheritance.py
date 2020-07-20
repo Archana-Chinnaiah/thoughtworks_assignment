b65a7269bd083f93a344f2ddc12f20bf5a9aa92b
Archana-Chinnaiah <66065120+Archana-Chinnaiah@users.noreply.github.com>
2020-07-20 19:54:41 +0530

    Update tax_for_product_using_inheritance.py

--- tax_for_product_using_inheritance.py
+++ tax_for_product_using_inheritance.py
@@ -8,7 +8,8 @@ class Product:
     def normal_tax(self):
          if(self.price>500):
              tax=(float(self.price)*0.05)
-             return tax         
+             return tax
+         
          else:
              tax=(float(self.price)*0.02)
              return tax
@@ -30,25 +31,29 @@ class TextileProduct(Product):
     def __init__(self,id,name,price,category):
         Product.__init__(self,id,name,price,category)
     def tax_for_textile(self):    
-        new_tax=Product.normal_tax(self)
-        tax=new_tax+(float(self.price)*0.01)
-        return tax
+        return (float(self.price)*0.01)
         
-         ''' GETTING USER INPUT '''
+        
+
+
+# USER INPUT
+
+print("\n\t\t****CALCULATING THE TAX FOR THE PRODUCT****\n")
+
 id=int(input("Enter the ID for the Product: "))
 name=input("Enter the NAME for the Product: ")
 price=float(input("Enter the PRICE for the Product: "))
-category=input("Enter the CATEGORY for the Product: ")
+category=input("Enter the CATEGORY for the Product(HOME NEEDS,PRODUCE,DAIRY,TEXTILES): ")
 
 
-if(category.lower()=="home needs" or category.lower()=="produce"):
-    product=Product(id,name,price,category)
-    print("The tax for the product",name,"is",product.normal_tax())
-elif(category.lower()=="dairy"):
+if(category.lower()=="dairy"):
     dairy=DairyProduct(id,name,price,category)
     print("The tax for the product",name,"is",dairy.dairy_tax())
-else:
+elif(category.lower()=="dairy"):
     textile=TextileProduct(id,name,price,category)
-    print("The Tax for the product",name," is",textile.tax_for_textile())
-    
+    print("The Tax for the product",name," is",textile.tax_for_textile()+textile.normal_tax())
+else:
+    product=Product(id,name,price,category)
+    print("The tax for the product",name,"is",product.normal_tax())
+
 
